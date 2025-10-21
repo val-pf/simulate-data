adec9c55b53749cc95a4dd81c57ce722a9b3e7ef
Val Pfeifer <vpfeifer@umkc.edu>
2025-10-21 12:39:03 -0500

    added comment

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -14,5 +14,8 @@ predictor <- rnorm(n = 100)
 # simulate response (y) variable with noise 
 response <- 2 * predictor + rnorm(n = length(predictor), sd = 0.2)
 
+# using length(predictor) is called dynamic coding, as opposed
+# to using 100, because it dynamically adapts to your predictor variable
+
 # plot the data
 plot (x = predictor, y = response)
